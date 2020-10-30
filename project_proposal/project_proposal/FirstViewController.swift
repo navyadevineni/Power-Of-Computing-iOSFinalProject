224a3cafa8c5b4884c8c1ab0266c1a5bf6e6e6bb
anshithavelagapudi <anshithavelagapudi@gmail.com>
2020-10-30 12:23:21 -0500

    Adding project proposal

---
+++ project_proposal/project_proposal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  project_proposal
+//
+//  Created by student on 10/23/20.
+//  Copyright © 2020 Northwest. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ project_proposal/project_proposal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  project_proposal
+//
+//  Created by student on 10/23/20.
+//  Copyright © 2020 Northwest. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
