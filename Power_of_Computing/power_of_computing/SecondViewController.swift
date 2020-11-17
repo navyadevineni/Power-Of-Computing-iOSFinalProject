ab40c34625d90e718a8e96dd33434ad56e446491
anshithavelagapudi <anshithavelagapudi@gmail.com>
2020-11-16 23:14:55 -0600

    added code for login view

--- Power_of_Computing/power_of_computing/SecondViewController.swift
+++ Power_of_Computing/power_of_computing/SecondViewController.swift
@@ -16,7 +16,20 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //This method is used for login view to app
+    @IBAction func submit(sender:Any){
+        if self.userNameTf.text == "" || self.userPasswordTf.text == "" {
+            
+            //Alert to tell the user that there was an error because they didn't fill anything in the textfields because they didn't fill anything in
+            
+            let alertController = UIAlertController(title: "Error", message: "Please enter an email and password.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        
+        }
 
 }
 
